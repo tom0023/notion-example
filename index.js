afd2030815f7e295d0f4eaa34269fc3d789c3bdb
tom0023 <tomita0720shinichi@gmail.com>
2021-07-06 18:35:05 +0900

    update テーブル情報呼び出し

--- index.js
+++ index.js
@@ -4,30 +4,43 @@ const notion = new Client({ auth: process.env.NOTION_KEY })
 
 const databaseId = process.env.NOTION_DATABASE_ID
 
-async function addItem(text) {
-  try {
-    await notion.request({
-      path: "pages",
-      method: "POST",
-      body: {
-        parent: { database_id: databaseId },
-        properties: {
-          title: { 
-            title:[
-              {
-                "text": {
-                  "content": text
-                }
-              }
-            ]
-          }
-        }
-      },
-    })
-    console.log("Success! Entry added.")
-  } catch (error) {
-    console.error(error.body)
+const response = await notion.databases.retrieve({
+  database_id: databaseId,
+  filter: {
+    property: 'Last ordered',
+    date: {
+      past_week: {},
+    },
   }
-}
+});
+console.log(response);
 
-addItem("Yurts in Big Sur, California")
+// データ挿入
+// async function addItem(text) {
+//   try {
+//     await notion.request({
+//       path: "pages",
+//       method: "POST",
+//       body: {
+//         parent: { database_id: databaseId },
+//         properties: {
+//           title: { 
+//             title:[
+//               {
+//                 "text": {
+//                   "content": text
+//                 }
+//               }
+//             ]
+//           }
+//         }
+//       },
+//     })
+//     console.log("Success! Entry added.")
+//   } catch (error) {
+//     console.error(error.body)
+//   }
+// }
+
+// addItem("追記テスト")
+
